749c968ed11902ef4ab2d0518133b05605af3fc3
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-04-22 06:08:29 +0200

    update run function to override terminal colors

--- tests/helpers/run.js
+++ tests/helpers/run.js
@@ -3,7 +3,11 @@ const { execSync } = require('child_process')
 function run(cmd) {
     return execSync(cmd, {
         encoding: 'utf-8',
-        env: {...process.env, NO_COLOR: '1'}
+        env: {
+            ...process.env, 
+            NO_COLOR: '1',
+            FORCE_COLOR: '0'
+        }
     }).trim()
 }
 
